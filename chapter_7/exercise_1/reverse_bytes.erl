719193cee5e3f1b306089da71c9cbd52efc75224
Stratus3D <admin@stratus3d.com>
2015-03-31 21:27:31 -0400

    Complete exercise 7.1.

--- chapter_7/exercise_1/reverse_bytes.erl
+++ chapter_7/exercise_1/reverse_bytes.erl
@@ -2,7 +2,8 @@
 
 -export([reverse/1]).
 
-% We take a binary
+% We take a binary and then need to reverse all the bytes. Reversing lists is
+% very efficient (and very easy) in Erlang. So we convert the binary to a list,
+% reverse the list, and the conver the list back to a binary.
 reverse(Binary) when is_binary(Binary) ->
-    % TODO: Return reversed binary
-    Binary.
+    list_to_binary(lists:reverse(binary_to_list(Binary))).
