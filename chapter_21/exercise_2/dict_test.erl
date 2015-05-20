29bddfa82dbcdb4087f30e3f5eaccfdd9cdc051a
Stratus3D <admin@stratus3d.com>
2015-05-20 07:41:42 -0400

    Start on exercise 21.2.

---
+++ chapter_21/exercise_2/dict_test.erl
@@ -0,0 +1,11 @@
+-module(dict_test).
+
+-export([test/0]).
+
+test() ->
+    % Create dictionary
+    % Add to dictionary
+    % Add another item to dictionary
+    % Fetch item from dictionary
+    % Find item in dictionary
+    % Delete item in dictionary
