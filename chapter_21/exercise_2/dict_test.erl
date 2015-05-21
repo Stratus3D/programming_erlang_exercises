492ac2352fc6b07ad037c0c0974292ed55a35170
Stratus3D <admin@stratus3d.com>
2015-05-21 06:44:37 -0400

    Complete exercise 21.2.

--- chapter_21/exercise_2/dict_test.erl
+++ chapter_21/exercise_2/dict_test.erl
@@ -4,8 +4,24 @@
 
 test() ->
     % Create dictionary
+    Dict = dict:new(),
+
     % Add to dictionary
+    Dict2 = dict:append(somekey, someval, Dict),
+
     % Add another item to dictionary
+    Dict3 = dict:append(anotherkey, anotherval, Dict2),
+
     % Fetch item from dictionary
+    _Value = dict:fetch(somekey, Dict3),
+
+    % Update item in dictionary
+    Dict4 = dict:update(somekey, fun (_) ->
+                                         new_elem
+                                 end, Dict3),
+
     % Find item in dictionary
+    {ok, _Value2} = dict:find(anotherkey, Dict4),
+
     % Delete item in dictionary
+    _Dict5 = dict:erase(anotherkey, Dict4).
