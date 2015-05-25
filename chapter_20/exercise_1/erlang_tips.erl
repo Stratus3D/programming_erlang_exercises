06d4691791f4f78280fc8b26f0ede3bb4ae0b3a1
Stratus3D <admin@stratus3d.com>
2015-05-25 14:25:12 -0400

    Add comments to exercise 20.1.

--- chapter_20/exercise_1/erlang_tips.erl
+++ chapter_20/exercise_1/erlang_tips.erl
@@ -10,14 +10,19 @@
         all_abuse/0, get_abuse/1, add_abuse/2, start_database/0,
         create_database/0, reset_tables/0]).
 
-% Primary API
+%%%===================================================================
+%%% Primary API
+%%%===================================================================
 all_users() ->
+    % Get all users in the user table
     do(qlc:q([X || X <- mnesia:table(user)])).
 
 get_user(Email) ->
+    % Select a user from the user table by email address
     do(qlc:q([X || X <- mnesia:table(user), X#user.email == Email])).
 
 add_user(Name, Email, Password) ->
+    % Create a user record and store it in the database
     User = #user{name=Name, email=Email, password=Password},
     F = fun() ->
                 mnesia:write(User)
@@ -25,12 +30,15 @@ add_user(Name, Email, Password) ->
     mnesia:transaction(F).
 
 all_tips() ->
+    % Get all tips in the tip table
     do(qlc:q([X || X <- mnesia:table(tip)])).
 
 get_tip(Url) ->
+    % Select a tip from the tip table by url
     do(qlc:q([X || X <- mnesia:table(tip), X#tip.url == Url])).
 
 add_tip(Url, Description, Date) ->
+    % Create a tip record and store it in the database
     Tip = #tip{url=Url, description=Description, date=Date},
     F = fun() ->
                 mnesia:write(Tip)
@@ -38,12 +46,15 @@ add_tip(Url, Description, Date) ->
     mnesia:transaction(F).
 
 all_abuse() ->
+    % Get all abuse records in the abuse table
     do(qlc:q([X || X <- mnesia:table(abuse)])).
 
 get_abuse(IpAddress) ->
+    % Select an abuse record from the abuse table by IP address
     do(qlc:q([X || X <- mnesia:table(abuse), X#abuse.ip_address == IpAddress])).
 
 add_abuse(IpAddress, NumVisits) ->
+    % Create an abuse record and store it in the database
     Abuse = #abuse{ip_address=IpAddress, num_visits=NumVisits},
     F = fun() ->
                 mnesia:write(Abuse)
@@ -51,6 +62,7 @@ add_abuse(IpAddress, NumVisits) ->
     mnesia:transaction(F).
 
 start_database() ->
+    % Get the database in a state where we can query it
     mnesia:start(),
     mnesia:wait_for_tables([user,tip,abuse], 20000).
 
@@ -71,11 +83,14 @@ create_database() ->
     ok.
 
 reset_tables() ->
+    % Empty all the tables in the database
     mnesia:clear_table(user),
     mnesia:clear_table(tip),
     mnesia:clear_table(abuse).
 
-% Private functions
+%%%===================================================================
+%%% Private functions
+%%%===================================================================
 do(Q) ->
     F = fun() -> qlc:e(Q) end,
     {atomic, Val} = mnesia:transaction(F),
