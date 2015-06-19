a9aba4836b72390050adb5dc935b3b32935a00ce
Stratus3D <admin@stratus3d.com>
2015-06-19 07:32:04 -0400

    WIP: Still working on counter.erl.

--- chapter_19/exercise_2/counter.erl
+++ chapter_19/exercise_2/counter.erl
@@ -1,9 +1,14 @@
 -module(counter).
 
--export([initialize/0, me/2]).
+-export([initialize/0, me/2, all_counters/0]).
 
 initialize() ->
    ets:new(counter, [set, public]).
 
 me(Module, Line) ->
-   ets:insert(counter, {{Module, Line}, 1}).
+    ets:insert(counter, {{Module, Line}, 1}).
+
+all_counters() ->
+    ets:foldl(fun(Item, Acc) ->
+                      [Item|Acc]
+              end, [], Tab).
