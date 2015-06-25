d536b78ff5df5642b59abdd34bbcf683e300482a
Stratus3D <admin@stratus3d.com>
2015-06-25 07:17:33 -0400

    WIP: More work on exercise 19.2.

--- chapter_19/exercise_2/count.erl
+++ chapter_19/exercise_2/count.erl
@@ -2,13 +2,27 @@
 
 -export([initialize/0, me/2, all_counters/0]).
 
+-define(TABLE_NAME, counter).
+
 initialize() ->
-   ets:new(counter, [set, public]).
+    % Create a new ets table
+    ets:new(?TABLE_NAME, [set, public]).
 
 me(Module, Line) ->
-    ets:insert(counter, {{Module, Line}, 1}).
+    % Keys are composed of modules and line numbers
+    Key = {Module, Line},
+
+    case ets:lookup(?TABLE_NAME, Key) of
+        [] ->
+            % Insert new element with a count of one if no elements match
+            ets:insert(?TABLE_NAME, {Key, 1});
+        _ ->
+            % We can use `update_counter` to update the counter if it exists
+            ets:update_counter(?TABLE_NAME, Key, {2, 1})
+    end.
 
 all_counters() ->
+    % We iterate over all the items in the table, adding them to a list.
     ets:foldl(fun(Item, Acc) ->
                       [Item|Acc]
-              end, [], Tab).
+              end, [], ?TABLE_NAME).
