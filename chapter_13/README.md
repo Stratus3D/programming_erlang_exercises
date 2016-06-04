1818d23bd1020d73fe422e7782c11d34df1c7051
Trevor Brown <admin@stratus3d.com>
2016-06-04 07:52:24 -0400

    Update exercise 13.2 with more comments.

--- chapter_13/README.md
+++ chapter_13/README.md
@@ -3,8 +3,10 @@
 ##1. Write a function named `my_spawn/3` that spawns a process and prints a message when it dies.
 
 ##2. Solve exercise 1 with the `on_exit` function shown in chapter 13.
+Solution in exercise_2/
 
 ##3. Write a function named `my_spawn/4` that spawns a process and kills it after it runs for more than the given number of seconds.
+Solution in exercise_3/
 
 ##4. Write a function that creates a registered process that prints "I'm still running" every 5 seconds. Create another function that monitors this function and restarts it if it dies.
 Solution in exercise_4/
--- chapter_13/exercise_2/on_exit.erl
+++ chapter_13/exercise_2/on_exit.erl
@@ -1,6 +1,8 @@
 -module(on_exit).
 -export([test/0, spawn_and_print_crash/3, test_spawn_and_print_crash/0]).
 
+% Function that will be spawned in our test. Automatically kills itself after
+% 5 seconds
 test() ->
   receive
     Msg ->
@@ -10,26 +12,36 @@ test() ->
       exit(self(), kill)
   end.
 
+% Spawn a process and add an on_exit handler function that prints out the
+% details of the crash
 spawn_and_print_crash(Module, Function, Args) ->
-  {Pid, Ref} = spawn(Module, Function, Args),
-  on_exit_print(Pid, now()).
+  Pid = spawn(Module, Function, Args),
+  StartTime = now(),
+  Handler = fun(Why) ->
+                TimeDifference = calculate_time_difference(now(), StartTime),
+                io:format("==================================================~n"),
+                io:format("Process ~w died in ~w millisconds with reason: ~w~n", [Pid, TimeDifference, Why]),
+                io:format("==================================================~n")
+            end,
+  on_exit(Pid, Handler).
 
-on_exit_print(Pid, StartTime) ->
+% The on_exit function that spawns a monitor and calls `Fun` when the process
+% dies
+on_exit(Pid, Fun) ->
   spawn(fun() ->
         Ref = monitor(process, Pid),
         receive
           {'DOWN', Ref, process, Pid, Why} ->
-            TimeDifference = calculate_time_difference(now(), StartTime),
-            io:format("==================================================~n"),
-            io:format("Process ~w died in ~w millisconds with reason: ~w~n", [Pid, TimeDifference, Why]),
-            io:format("==================================================~n")
+                Fun(Why)
         end
   end).
 
+% Utility function for calculating how long the process lived
 calculate_time_difference(Current, Past) ->
   {Mega, Secs, _} = Current,
   {PastMega, PastSecs, _} = Past,
   (Mega * 1000000 + Secs) - (PastMega * 1000000 + PastSecs).
 
+% Function to test this behavior
 test_spawn_and_print_crash() ->
   ?MODULE:spawn_and_print_crash(?MODULE, test, []).
