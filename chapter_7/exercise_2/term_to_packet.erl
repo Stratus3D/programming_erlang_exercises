b476a5b23a0d0363225df6d6c799f17801ae35b3
Trevor Brown <admin@stratus3d.com>
2016-12-26 10:34:12 -0500

    Fix mistakes in exercises 7.2 and 7.3.

--- chapter_7/exercise_2/term_to_packet.erl
+++ chapter_7/exercise_2/term_to_packet.erl
@@ -2,9 +2,11 @@
 
 -export([term_to_packet/1]).
 
+-define(LENGTH_HEADER, 32). % 8 (byte) * 4
+
 term_to_packet(Term) ->
     BinTerm = term_to_binary(Term), % The payload
     Length = byte_size(BinTerm), % The value for the 4 byte header
     % Then we construct the binary. First we set the length as the first 4 bytes.
     % Then the BinTerm binary with the payload.
-    <<Length:4, BinTerm/binary>>.
+    <<Length:?LENGTH_HEADER/integer, BinTerm/binary>>.
--- chapter_7/exercise_3/packet_to_term.erl
+++ chapter_7/exercise_3/packet_to_term.erl
@@ -2,7 +2,11 @@
 
 -export([packet_to_term/1]).
 
+-define(LENGTH_HEADER, 32). % 8 (byte) * 4
+
 packet_to_term(Packet) ->
-    <<Length:4/integer, BinTerm/binary>> = Packet,
-    erlang:display(Length),
+    % Extract the length from the head of the binary
+    <<Length:?LENGTH_HEADER/integer, BinTerm:Length/binary>> = Packet,
+    % Take the packet payload portion of the  binary and convert it back to an
+    % Erlang term
     binary_to_term(BinTerm).
