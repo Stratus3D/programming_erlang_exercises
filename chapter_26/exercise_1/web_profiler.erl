027e25d1ba1156fb9b0bf5eca413a34b097fa402
Trevor Brown <admin@stratus3d.com>
2020-07-04 09:11:46 -0400

    Complete exercise 26.1

---
+++ chapter_26/exercise_1/web_profiler.erl
@@ -0,0 +1,32 @@
+-module(web_profiler).
+
+-export([ping/2, receive_response/2]).
+
+ping(URL, Timeout) ->
+    {_Protocol, Host, Port, Path} = parse_url(URL),
+    {ok, Socket} = gen_tcp:connect(Host, Port, [binary, {packet, 0}, {active, false}]),
+    % Send the request
+    ok = gen_tcp:send(Socket, io_lib:format("HEAD ~s HTTP/1.0\r\n\r\n", [Path])),
+    % Time the response
+    {Time, Result} = timer:tc(fun receive_response/2, [Socket, Timeout]),
+
+    % Format the return value of the function
+    case Result of
+        timeout ->
+            timeout;
+        _ ->
+            {time, Time}
+    end.
+
+receive_response(Socket, Timeout) ->
+    % And receive the response
+    case gen_tcp:recv(Socket, 0, Timeout) of
+        {ok, Packet} -> Packet;
+        {error, timeout} -> timeout
+    end.
+
+parse_url(Url) ->
+    {ok, Parsed} = http_uri:parse(Url),
+    % We ignore the query string for simplicity here
+    {Protocol, _, Host, Port, Path, _Query} = Parsed,
+    {Protocol, Host, Port, Path}.
