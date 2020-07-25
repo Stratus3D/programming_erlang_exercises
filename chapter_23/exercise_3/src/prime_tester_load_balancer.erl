a60a7d8faa11d9c379067fce98327c2756498d5f
Trevor Brown <admin@stratus3d.com>
2020-07-25 10:02:13 -0400

    Fix mistakes in handle_down_worker for exercise 23.3

--- chapter_23/exercise_3/src/prime_tester_load_balancer.erl
+++ chapter_23/exercise_3/src/prime_tester_load_balancer.erl
@@ -22,8 +22,7 @@
 
 -define(SERVER, ?MODULE).
 
--record(state, {limit=0,
-                free_pids=[],
+-record(state, {free_pids=[],
                 jobs=#{},
                 queue=queue:new()}).
 
@@ -128,16 +127,24 @@ code_change(_OldVsn, State, _Extra) ->
 %%% Internal functions
 %%%===================================================================
 
-handle_down_worker(Pid, State = #state{limit=Limit, jobs=Pids, queue=Queue}) ->
-    io:format("got worker down"),
+handle_down_worker(Pid, State = #state{free_pids=FreePids, jobs=Jobs, queue=Queue}) ->
+    io:format("Got worker down. Sending error back to caller~n"),
+    % Send error back to the caller
+    {From, Number} = maps:get(Pid, Jobs),
+    gen_server:reply(From, {error, {failed_to_check, Number}}),
+
+    % Start a new worker in its place
+    {ok, NewPid} = supervisor:start_child(prime_tester_worker_sup, []),
+    _Ref = erlang:monitor(process, NewPid),
+
     case queue:out(Queue) of
         {{value, {From, Number}}, NewQueue} ->
-            {ok, Pid} = supervisor:start_child(prime_tester_worker_sup, [Number]),
-            NewPid = erlang:monitor(process, Pid),
-            NewPids = gb_sets:insert(NewPid, maps:remove(Pid,Pids)),
-            gen_server:reply(From, {ok, Pid}),
-            {noreply, State#state{jobs=NewPids, queue=NewQueue}};
+            % Start the new job
+            ok = prime_tester_worker:is_prime(NewPid, Number),
+            NewJobs = maps:put(NewPid, {From, Number}, maps:remove(Pid, Jobs)),
+
+            {noreply, State#state{jobs=NewJobs, queue=NewQueue}};
         {empty, _} ->
             % If nothing in the queue continue as before
-            {noreply, State#state{limit=Limit+1, jobs=gb_sets:delete(Pid, Pids)}}
+            {noreply, State#state{free_pids=[NewPid|FreePids], jobs=maps:remove(Pid, Jobs)}}
     end.
